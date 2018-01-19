fcae8874395ea5a580740d0a823c431546d9504c
siria3007 <siria3007@FHC-IC0034080.wrdsb.ca>
2018-01-19 14:21:54 -0500

    Dice scratch integrated into the attack screen. Button shortcut attack screen on main menu

--- core/src/gdx/game/screens/ScrGamAttack.java
+++ core/src/gdx/game/screens/ScrGamAttack.java
@@ -13,13 +13,15 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import gdx.game.GamMain;
 import gdx.game.objects.Button;
+import java.util.Arrays;
 import java.util.Random;
 
 /**
  *
  * @author brauj1894
  */
-public class ScrGamAttack implements Screen{
+public class ScrGamAttack implements Screen {
+
     Random ranGen = new Random();
     int nRandAttack, nRandDefend;
     SpriteBatch batch;
@@ -27,6 +29,10 @@ public class ScrGamAttack implements Screen{
     Sprite sprBG;
     Texture txtBG, txtAttack, txtDefend, txtWhite1, txtWhite2;
     Button btnEndBattle, btnBattle;
+    Texture txtAttackerDiceRoll, txtDefenderDiceRoll;
+    int arnDiceRanAtt[] = new int[3];
+    int arnDiceRanDef[] = new int[2];
+
     public ScrGamAttack(GamMain _game) {
         game = _game;
         txtBG = new Texture("bg3.jpg");
@@ -37,9 +43,12 @@ public class ScrGamAttack implements Screen{
         sprBG = new Sprite(txtBG, 0, 0, 1144, 744);
         btnBattle = new Button(500, 650, 90, 40, "button_battle.png");
         btnEndBattle = new Button(500, 100, 167, 48, "button_end-battle.png");
+        txtAttackerDiceRoll = new Texture("button_attacker-dice-roll.png");
+        txtDefenderDiceRoll = new Texture("button_defender-dice-roll.png");
+        btnBattle = new Button(500, 650, 90, 40, "button_battle.png");
         batch = new SpriteBatch();
     }
-    
+
     @Override
     public void render(float delta) {
         checkButtons();
@@ -49,6 +58,8 @@ public class ScrGamAttack implements Screen{
         batch.draw(txtWhite1, 325, 650);
         batch.draw(txtDefend, 800, 650);
         batch.draw(txtWhite2, 933, 650);
+        batch.draw(txtAttackerDiceRoll, 20, 400);
+        batch.draw(txtDefenderDiceRoll, 700, 400);
         btnBattle.draw(batch);
         btnEndBattle.draw(batch);
 
@@ -62,6 +73,16 @@ public class ScrGamAttack implements Screen{
         font = new BitmapFont();
         font.draw(spriteBatch, sNumberAttackers, 341, 670);
         font.draw(spriteBatch, sNumberDefenders, 950, 670);
+        CharSequence sDA1 = Integer.toString(arnDiceRanAtt[0]);
+        CharSequence sDA2 = Integer.toString(arnDiceRanAtt[1]);
+        CharSequence sDA3 = Integer.toString(arnDiceRanAtt[2]);
+        CharSequence sDD1 = Integer.toString(arnDiceRanDef[0]);
+        CharSequence sDD2 = Integer.toString(arnDiceRanDef[1]);
+        font.draw(spriteBatch, sDA1, 350, 425);
+        font.draw(spriteBatch, sDA2, 380, 425);
+        font.draw(spriteBatch, sDA3, 410, 425);
+        font.draw(spriteBatch, sDD1, 1020, 425);
+        font.draw(spriteBatch, sDD2, 1050, 425);
         spriteBatch.end();
     }
     boolean isBattle = false;
@@ -72,73 +93,74 @@ public class ScrGamAttack implements Screen{
         if (Gdx.input.justTouched()) {
             if (btnEndBattle.isMousedOver()) {
                 game.changeScreen(0);
-            } 
+            }
             if (isTroopsA && isTroopsD) {
                 if (btnBattle.isMousedOver()) {
-                    nRandAttack = ranGen.nextInt(10);
-                    System.out.println(nRandAttack);
-
-                    nRandDefend = ranGen.nextInt(10);
-                    System.out.println(nRandDefend);
-                    isBattle = true;
+                    diceMechanics();
                 }
-
-                if (isBattle) {
-                    if (nRandAttack > nRandDefend) {
-                        System.out.println("Defenders lost one");
-                        nTroopsD -= 1;
-                        System.out.println(nTroopsD);
-                        if (nTroopsD == 0) {
-                            isTroopsD = false;
-                        }
-                    } else if (nRandAttack < nRandDefend) {
-                        System.out.println("Attackers lost one");
-                        nTroopsA -= 1;
-                        System.out.println(nTroopsA);
-                        if (nTroopsA == 0) {
-                            isTroopsA = false;
-                        }
-                    } else if (nRandAttack == nRandDefend) {
-                        System.out.println("Nobody dies this night");
-                    }
-                    if (isTroopsA == false) {
-                        System.out.println("The defenders win this War");
-                        game.changeScreen(0);
-                    } else if (isTroopsD == false) {
-                        System.out.println("The attackers have won");
-                        game.changeScreen(0);
-                    }
-                }
+                
+            }else{
+                game.changeScreen(0);
+            }
+            if(nTroopsA <= 0){
+                isTroopsA = false;
+            }
+            if(nTroopsD <= 0){
+                isTroopsD = false;
             }
         }
     }
+    private void diceMechanics() {
+        for (int i = 0; i < 3; i++) {
+            arnDiceRanAtt[i] = ranGen.nextInt(6) + 1;
+        }
+        for (int j = 0; j < 2; j++) {
+            arnDiceRanDef[j] = ranGen.nextInt(6) + 1;
+        }
+        Arrays.sort(arnDiceRanAtt);
+        Arrays.sort(arnDiceRanDef);
+        System.out.println(arnDiceRanAtt);
+        if (arnDiceRanAtt[2] <= arnDiceRanDef[1]) {
+            nTroopsA -=1;
+            System.out.println(nTroopsA);
+        } else {
+            nTroopsD -=1;
+            System.out.println(nTroopsD);
+        }
+        if (arnDiceRanAtt[1] <= arnDiceRanDef[0]) {
+            nTroopsA -=1;
+            System.out.println(nTroopsA);
+        } else {
+            nTroopsD -=1;
+            System.out.println(nTroopsD);
+        }
+        System.out.println(arnDiceRanAtt[1]);
+        System.out.println(arnDiceRanAtt[2]);
+        System.out.println(arnDiceRanDef[0]);
+        System.out.println(arnDiceRanDef[1]);
+    }
+
     @Override
     public void show() {
-        
     }
 
     @Override
     public void resize(int width, int height) {
-        
     }
 
     @Override
     public void pause() {
-        
     }
 
     @Override
     public void resume() {
-        
     }
 
     @Override
     public void hide() {
-        
     }
 
     @Override
     public void dispose() {
-        
     }
 }
